96291d789d1a8d97fd60bf7466f3cad4d2449f27
StampoulisSpiros <Darkdream3r>
2020-12-25 19:15:15 +0100

    some more configuration

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,1 +1,18 @@
-console.log("contentScript.js");
+import {getTTFB, getLCP, getFID, getCLS, getFCP} from 'web-vitals';
+
+const infoDiv = document.createElement('div');
+infoDiv.style.position = 'fixed';
+infoDiv.style.left = '0';
+infoDiv.style.top = '0';
+infoDiv.style.zIndex = '0';
+infoDiv.style.backgroundColor = 'black';
+infoDiv.style.position = 'white';
+infoDiv.style.position = '1rem';
+infoDiv.style.fontFamily = 'Arial';
+document.body.appendChild(infoDiv);
+
+getTTFB(console.log);
+getLCP(console.log);
+getFID(console.log);
+getFCP(console.log);
+getCLS(console.log);
