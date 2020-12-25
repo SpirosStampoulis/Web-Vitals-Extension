fe069159cd9c14af9033edc5ba3c17369bd72874
StampoulisSpiros <Darkdream3r>
2020-12-25 22:07:04 +0100

    final commit

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,18 +1,64 @@
-import {getTTFB, getLCP, getFID, getCLS, getFCP} from 'web-vitals';
+import { getTTFB, getLCP, getFID, getFCP, getCLS } from "web-vitals";
+import { InfluxDB, Point } from "@influxdata/influxdb-client-browser";
 
-const infoDiv = document.createElement('div');
-infoDiv.style.position = 'fixed';
-infoDiv.style.left = '0';
-infoDiv.style.top = '0';
-infoDiv.style.zIndex = '0';
-infoDiv.style.backgroundColor = 'black';
-infoDiv.style.position = 'white';
-infoDiv.style.position = '1rem';
-infoDiv.style.fontFamily = 'Arial';
+// You can generate a Token from the "Tokens Tab" in the UI
+const token = ''
+const org = ''
+const bucket = ''
+
+const client = token
+  ? new InfluxDB({
+      url: "https://eu-central-1-1.aws.cloud2.influxdata.com",
+      token: token,
+    })
+  : null;
+
+const infoDiv = document.createElement("div");
+infoDiv.style.position = "fixed";
+infoDiv.style.left = 0;
+infoDiv.style.top = 0;
+infoDiv.style.zIndex = 0;
+infoDiv.style.backgroundColor = "black";
+infoDiv.style.color = "white";
+infoDiv.style.padding = "1rem";
+infoDiv.style.fontFamily = "Arial";
 document.body.appendChild(infoDiv);
 
-getTTFB(console.log);
-getLCP(console.log);
-getFID(console.log);
-getFCP(console.log);
-getCLS(console.log);
+const metrics = {};
+const gatherMetrics = ({ name, value }) => {
+  metrics[name] = value;
+
+  if (client) {
+    const writeApi = client.getWriteApi(org, bucket);
+    writeApi.useDefaultTags({ host: "host1" });
+
+    const point = new Point("perf").floatField(name, value);
+    writeApi.writePoint(point);
+    writeApi.close();
+  }
+
+  chrome.runtime.sendMessage({
+    type: "performance:metric",
+    name,
+    value,
+  });
+
+  const metricsHTML = Object.keys(metrics)
+    .map((k) => `<div>${k}</div><div>${Math.round(metrics[k])}</div>`)
+    .join("");
+
+  infoDiv.innerHTML = `
+<div style="font-weight:bold;font-size:x-large">Perf Metrics</div>
+<div style="display:grid; grid-template-columns: 1fr 1fr; grid-column-gap: 1rem;">
+  <div>Metric</div>
+  <div>Value</div>
+  ${metricsHTML}
+</div>
+  `;
+};
+
+getTTFB(gatherMetrics);
+getLCP(gatherMetrics);
+getFID(gatherMetrics);
+getFCP(gatherMetrics);
+getCLS(gatherMetrics);
